1ab5009f78e213549d32e30a7e0e6435c519fe12
Raymond Huang <shanhua18@gmail.com>
2024-06-02 22:02:44 -0400

    add backgtacking

--- Backtracking - Permutations.py
+++ Backtracking - Permutations.py
@@ -16,3 +16,26 @@ class Solution:
             nums.append(n)
         
         return res
+    
+
+
+    class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ml = []
+
+        def permutations(i):
+            if i == len(nums):
+                sl = []
+                for n in nums:
+                    sl.append(n)
+
+                ml.append(sl.copy())
+                return
+
+            for j in range(i, len(nums)):
+                nums[i], nums[j] = nums[j], nums[i]
+                permutations(i + 1)
+                nums[i], nums[j] = nums[j], nums[i]
+
+        permutations(0)
+        return ml
