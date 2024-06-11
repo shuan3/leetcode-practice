47ebc99eead0d261bda538a4340a90b321a0209a
Raymond Huang <shanhua18@gmail.com>
2024-06-10 22:10:54 -0400

    udpated

--- Backtracking - Permutations.py
+++ Backtracking - Permutations.py
@@ -77,3 +77,20 @@ class Solution:
         result = [] 
         dfs([], [False] * len(nums)) 
         return result 
+    
+
+class Solution:
+    def permute(self,nums):
+        n=len(nums)
+        ans,sol=[],[]
+        def backtrack():
+            if len(sol)==n:
+                ans.append(sol[:])
+                return
+            for x in nums:
+                if x not in sol:
+                    sol.append(x)
+                    backtrack()
+                    sol.pop()
+        backtrack()
+        return ans
